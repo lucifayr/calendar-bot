803fd31bc5e0389714b46fa24f791b4d9c63fab5
Jackboxx <57196053+Jackboxx@users.noreply.github.com>
2021-12-01 09:34:49 +0100

    Add files via upload

---
+++ events/messageCreate.js
@@ -0,0 +1,12 @@
+module.exports = (client, message) => {
+    const prefix = client.config.prefix;
+    if ((message.content.indexOf(prefix) !== 0) || (message.author.bot) || (message.channel.type === 'dm') || (message.channel.type === 'GROUP_DM')) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
+
+    if (!cmd) return;
+
+    cmd.execute(client, message, args);
+}
