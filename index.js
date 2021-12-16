972f00aad8c383577a4bca809ac6763c7b3c7e28
roteKlaue <87981510+roteKlaue@users.noreply.github.com>
2021-12-16 07:56:00 +0100

    small clean up
    small clean up of the index file

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const Discord = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const fs = require("fs");
 
-const Client = new Discord.Client({
+const client = new Client({
     intents: [
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
     ]
 });
 
-Client.config = require("./config.js");
-Client.commands = new Discord.Collection();
-Client.eventIDList = [];
-Client.messages_send = [];
-Client.__first_run__ = true;
-Client.end_date_saved;
-Client.start_date_saved;
-Client.guild;
-Client.channel;
-Client.listeners_active = false;
+client.config = require("./config.js");
+client.commands = new Collection();
+client.eventIDList = [];
+client.messages_send = [];
+client.__first_run__ = true;
+client.end_date_saved;
+client.start_date_saved;
+client.guild;
+client.channel;
+client.listeners_active = false;
 
 const commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const events = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
@@ -25,13 +25,13 @@ const events = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
 for (const file of commands) {
     const command = require(`./commands/${file}`);
     console.log(`Loading command ${file} as ${command.name}`);
-    Client.commands.set(command.name.toLowerCase(), command);
+    client.commands.set(command.name.toLowerCase(), command);
 };
 
 for (const file of events) {
     console.log(`Loading discord.js event ${file}`);
     const event = require(`./events/${file}`);
-    Client.on(file.split(".")[0], event.bind(null, Client));
+    client.on(file.split(".")[0], event.bind(null, client));
 };
 
-Client.login(Client.config.token);
+client.login(client.config.token);
